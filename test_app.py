469695cf12f7295b5ac477ee3f81ef033a10103c
AvshRLev <avsha.earth@gmail.com>
2021-07-26 16:15:30 +0300

    old tests work without server running

--- test_app.py
+++ test_app.py
@@ -16,38 +16,38 @@ class TestAPI(unittest.TestCase):
 
     def test_get_valid_package_and_version(self):
         response = self.app.get(BASE + 'find-up/3.0.0')
-        self.assertEqual(response.json()['deps'], {'locate-path': '^3.0.0'})
+        self.assertEqual(response.get_json()['deps'], {'locate-path': '^3.0.0'})
 
     def test_get_valid_namespace_package_and_version(self):
         response = self.app.get(BASE + '@babel/types/^7.4.0')
-        self.assertEqual(response.json()['devDeps'], {'@babel/generator': '^7.4.0', '@babel/parser': '^7.4.0'})
+        self.assertEqual(response.get_json()['devDeps'], {'@babel/generator': '^7.4.0', '@babel/parser': '^7.4.0'})
 
     def test_get_invalid_package(self):
         response = self.app.get(BASE + 'qqqqqq/3.0.0')
-        self.assertEqual(response.text, 'Not Found')
+        self.assertEqual(response.get_data(), b'Not Found')
 
     def test_get_invalid_version(self):
         response = self.app.get(BASE + 'find-up/100.100.100')
-        self.assertEqual(response.text, 'version not found: 100.100.100')
+        self.assertEqual(response.get_data(), b'version not found: 100.100.100')
 
     def test_get_invalid_name_space(self):
         response = self.app.get(BASE + '@bbbbbbb/types/^7.4.0')
-        self.assertEqual(response.text, 'Not Found')
+        self.assertEqual(response.get_data(), b'Not Found')
 
     def test_clean_version(self):
-        version = app.clean_version('^0.0.0')
+        version = main.clean_version('^0.0.0')
         self.assertEqual(version, '0.0.0')
 
-        version = app.clean_version('~0.0.0')
+        version = main.clean_version('~0.0.0')
         self.assertEqual(version, '0.0.0')
 
-        version = app.clean_version('>0.0.0')
+        version = main.clean_version('>0.0.0')
         self.assertEqual(version, 'latest')
 
-        version = app.clean_version('*')
+        version = main.clean_version('*')
         self.assertEqual(version, 'latest')
 
-        version = app.clean_version('0.0.x')
+        version = main.clean_version('0.0.x')
         self.assertEqual(version, '0.0.0')
 
 
