d28ec5a34f2215a5eef6d7fee492d5a7dd998748
AvshRLev <avsha.earth@gmail.com>
2021-07-26 09:24:34 +0300

    tests on live app with real api calls

--- test_app.py
+++ test_app.py
@@ -16,7 +16,39 @@ class TestAPI(unittest.TestCase):
     def test_get_valid_package_and_version(self):
         response = requests.get(BASE + 'find-up/3.0.0')
         self.assertEqual(response.json()['deps'], {'locate-path': '^3.0.0'})
-        
+
+    def test_get_valid_namespace_package_and_version(self):
+        response = requests.get(BASE + '@babel/types/^7.4.0')
+        self.assertEqual(response.json()['devDeps'], {'@babel/generator': '^7.4.0', '@babel/parser': '^7.4.0'})
+
+    def test_get_invalid_package(self):
+        response = requests.get(BASE + 'qqqqqq/3.0.0')
+        self.assertEqual(response.text, 'Not Found')
+
+    def test_get_invalid_version(self):
+        response = requests.get(BASE + 'find-up/100.100.100')
+        self.assertEqual(response.text, 'version not found: 100.100.100')
+
+    def test_get_invalid_name_space(self):
+        response = requests.get(BASE + '@bbbbbbb/types/^7.4.0')
+        self.assertEqual(response.text, 'Not Found')
+
+    def test_clean_version(self):
+        version = app.clean_version('^0.0.0')
+        self.assertEqual(version, '0.0.0')
+
+        version = app.clean_version('~0.0.0')
+        self.assertEqual(version, '0.0.0')
+
+        version = app.clean_version('>0.0.0')
+        self.assertEqual(version, 'latest')
+
+        version = app.clean_version('*')
+        self.assertEqual(version, 'latest')
+
+        version = app.clean_version('0.0.x')
+        self.assertEqual(version, '0.0.0')
+
 
 if __name__ == "__main__":
     unittest.main()
