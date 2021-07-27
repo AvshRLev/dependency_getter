0bc187d05038b5e3cff2af677cb6d3d9cc8cd012
AvshRLev <avsha.earth@gmail.com>
2021-07-27 19:32:53 +0300

    a commit

--- main.py
+++ main.py
@@ -3,28 +3,33 @@ from flask.templating import render_template
 import redis
 import requests
 import json
+import os
 
 redis_host = 'localhost'
 redis_port = 6379
-
 redis_client = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
 
+npm_base_url = os.environ['NPM_BASE_URL']
 
 app = Flask(__name__)
 
 @app.route("/",  methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return render_template(index.html)
+        return render_template('index.html')
+    if request.method == 'POST':
+        package_name = request.form.get('package_name')
+        package_version = request.form.get('pacakage_version')
+        dependencies = requests.get
         
 @app.route("/<string:package>/<string:version>", methods=['GET'])
 def get_dep(package, version):
     version = clean_version(version)
     response = get_from_cache(f'{package}/{version}')
     if response is None:
-        response = get_from_node_api(f'https://registry.npmjs.org/{package}/{version}')
-        if 'description' not in response.json():
-            return response.json()
+        response = get_from_node_api(f'{npm_base_url}/{package}/{version}')
+        if response.status_code != 200:
+            return {"code": response.status_code, "response": response.json()}
         cache_for_one_day(f'{package}/{version}', response)
         return extract_deps(response.json())
     return extract_deps(json.loads(response))
@@ -35,9 +40,9 @@ def get_namespace_dep(namespace, package, version):
     version = clean_version(version)
     response = get_from_cache(f'{namespace}/{package}/{version}')
     if response is None:
-        response = get_from_node_api(f'https://registry.npmjs.org/{namespace}/{package}/{version}')
-        if 'description' not in response.json():
-            return response.json() 
+        response = get_from_node_api(f'{npm_base_url}/{namespace}/{package}/{version}')
+        if response.status_code != 200:
+            return {"code": response.status_code, "response": response.json()}
         cache_for_one_day(f'{namespace}/{package}/{version}', response)
         return extract_deps(response.json())
     return extract_deps(json.loads(response))
--- test_app.py
+++ test_app.py
@@ -1,9 +1,12 @@
 import unittest
 from unittest import mock
+from unittest.mock import Mock, patch
 import requests
 import redis
 import main
 import json
+from main import get_from_cache, get_from_node_api
+
 
 redis_host = 'localhost'
 redis_port = 6379
@@ -11,15 +14,38 @@ redis_client = redis.StrictRedis(host=redis_host, port=redis_port, decode_respon
 
 BASE = 'http://127.0.0.1:5000/'
 
+
+
 class TestAPI(unittest.TestCase):
     def setUp(self):
         main.app.testing = True
         self.app = main.app.test_client()
-        redis_client.del('find-up/3.0.0')
+        redis_client.delete('find-up/3.0.0')
+        
 
     def tearDown(self):
         pass
+
+    mock_response = json.dumps({"name":"@types/ms","version":"0.7.31","description":"TypeScript definitions for ms", "dependencies": "{'locate-path': '^3.0.0'}"})
     
+    @patch('main.get_from_node_api')
+    def test_get_from_node_api(self, mock_get_from_node):
+        mock_cache = [{"name":"lodash","version":"4.8.0"}]
+        mock_get_from_node.return_value = Mock(ok=True)
+        mock_get_from_node.status_code = 200
+        mock_get_from_node.return_value.json.return_value = mock_cache
+        response = self.app.get(BASE + 'find/3.0.0')
+        self.assertEqual(response, mock_cache)
+    
+
+    # @patch('main.get_from_cache')
+    # def test_get_from_cache(self, mock_get_from_cache):
+    #     mock_cache = '{"name":"lodash","version":"4.8.0"}'
+    #     mock_get_from_cache.return_value.ok = True
+    #     mock_get_from_cache.return_value.json.return_value = mock_cache
+    #     response = self.app.get(BASE + 'find/3.0.0')
+    #     self.assertEqual(response, json.dumps(mock_cache))
+        
     
     def test_get_valid_package_and_version(self):
         response = self.app.get(BASE + 'find-up/3.0.0')
@@ -31,15 +57,15 @@ class TestAPI(unittest.TestCase):
 
     def test_get_invalid_package(self):
         response = self.app.get(BASE + 'qqqqqq/3.0.0')
-        self.assertEqual(response.get_data(), b'Not Found')
+        self.assertEqual(response.get_json(), {'code': 404, 'response': 'Not Found'} )
 
     def test_get_invalid_version(self):
         response = self.app.get(BASE + 'find-up/100.100.100')
-        self.assertEqual(response.get_data(), b'version not found: 100.100.100')
+        self.assertEqual(response.get_json(), {'code': 404, 'response': 'version not found: 100.100.100'})
 
     def test_get_invalid_name_space(self):
         response = self.app.get(BASE + '@bbbbbbb/types/^7.4.0')
-        self.assertEqual(response.get_data(), b'Not Found')
+        self.assertEqual(response.get_json(),  {'code': 404, 'response': 'Not Found'} )
 
     def test_clean_version(self):
         version = main.clean_version('^0.0.0')
