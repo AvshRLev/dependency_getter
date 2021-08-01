74f12cbbde5f02a4af7570b239b361f2be6d7a06
AvshRLev <avsha.earth@gmail.com>
2021-08-01 16:29:21 +0300

    move redis client to redis.py

--- data/redis.py
+++ data/redis.py
@@ -1,7 +1,13 @@
 import json
+import os
+import redis
 
-def cache_for_one_day(path_string, response, redis_client):
+redis_host = os.environ.get('REDIS', default='localhost')
+redis_port = 6379
+redis_client = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
+
+def cache_for_one_day(path_string, response):
     redis_client.setex(path_string , 86400, json.dumps(response.json()))
 
-def get_from_cache(path_string, redis_client):
+def get_from_cache(path_string):
     return redis_client.get(path_string)
